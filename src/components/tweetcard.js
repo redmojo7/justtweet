a343d4d02097849ed4e8faadc438ceb2a3086537
redmojo <donaldchoi5180@gmail.com>
2023-05-09 08:45:38 +0800

    update delete tweet logic

--- src/components/tweetcard.js
+++ src/components/tweetcard.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisVertical, faComment, faRetweet, faHeart, faEye } from '@fortawesome/free-solid-svg-icons'
 
 class TweetCards extends Component {
-    
+
     state = {
         cards: []
     }
@@ -25,7 +25,7 @@ class TweetCards extends Component {
 
     componentDidUpdate(prevProps) {
         if (this.props.cards !== prevProps.cards) {
-          this.setState({ cards: this.props.cards });
+            this.setState({ cards: this.props.cards });
         }
     }
 
@@ -64,9 +64,20 @@ class TweetCards extends Component {
 
     handleEllipsis(id) {
         console.log("Ellipsis Clicked", id);
-        this.setState({
-            showDeleteButton: true
+        //const showDeleteButton = !this.state.showDeleteButton;
+        const cards = this.state.cards.map(item => {
+            if (item.id === id) {
+                if (item.showDeleteButton === undefined) {
+                    item.showDeleteButton = true;
+                } else {
+                    item.showDeleteButton = !item.showDeleteButton;
+                }
+            } else {
+                item.showDeleteButton = false;
+            }
+            return item;
         });
+        this.setState({ cards: cards});
     }
 
     handleDelete(id) {
@@ -103,17 +114,11 @@ class TweetCard extends Component {
         this.state = {
             showDeleteButton: false,
         };
-        this.handleEllipsis = this.handleEllipsis.bind(this);
+        //this.handleEllipsis = this.handleEllipsis.bind(this);
     }
-    handleEllipsis(id) {
-        console.log('Ellipsis Clicked', id);
-        // your code to delete the tweet goes here
-        this.setState({
-            showDeleteButton: !this.setState.showDeleteButton,
-        });
-    }
+
     render(props) {
-        const { content, comments, retweets, likes, views, date, user } = this.props.card;
+        const { content, comments, retweets, likes, views, date, user, showDeleteButton} = this.props.card;
         return (
             <div className="container">
                 <div className="row">
@@ -121,8 +126,11 @@ class TweetCard extends Component {
                         <img src={require(`../images/${user.avatar}`)} className="avarta-image" alt="avarta Image" />
                     </div>
                     <div className="col-md-10">
-                        <strong>{user.name}</strong> @{user.account} - {date}<FontAwesomeIcon className="float-end" onClick={() => this.handleEllipsis(this.props.card.id)} icon={faEllipsisVertical} />
-                        {this.state.showDeleteButton &&
+                        <strong>{user.name}</strong> @{user.account} - {date}
+                        <div className="float-end ml-2 p-2 py-1" onClick={() => this.props.onEllipsis(this.props.card.id)}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </div>
+                        {showDeleteButton &&
                             <button className="btn btn-danger float-end" onClick={() => this.props.onDelete(this.props.card.id)}>Delete</button>
                         }
                         <p>{content}</p>
